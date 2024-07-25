4302c505d4f2e7a10bfa554bfa44e69d6239fa80
clwmfksek <139228254+clwmfksek@users.noreply.github.com>
2024-07-25 22:25:51 +0900

    [Bronze III] Title: 오븐 시계, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,16 @@
+h,m = map(int,input().split(" "))
+t = int(input())
+
+if t < 60 : 
+    m += t
+else :
+    h += t//60
+    m += t%60
+
+if m >= 60 :
+    h += 1
+    m -= 60
+
+if h >= 24 :
+    h -= 24
+print(f"{h} {m}")
