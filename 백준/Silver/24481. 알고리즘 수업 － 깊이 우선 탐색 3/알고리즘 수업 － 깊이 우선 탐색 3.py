40fab68b8e622d1c352ecd84601a23989c72e872
clwmfksek <139228254+clwmfksek@users.noreply.github.com>
2024-07-25 22:49:21 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 740 ms, Memory: 80080 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+n,m,r = map(int,input().split())
+visited = [-1] * (n+1)
+graph = [[] for _ in range(m+2)]
+for i in range(m):
+    num1,num2 = map(int,input().split())
+    graph[num1].append(num2)
+    graph[num2].append(num1)
+
+visited[r] = 0
+def dfs(v,e,r):
+    for i in sorted(e[r]):
+        if(v[i] == -1):
+            visited[i] = visited[r] + 1
+            dfs(v,e,i)
+
+dfs(visited,graph,r)
+for i in range(1,len(visited)):
+    print(visited[i])
