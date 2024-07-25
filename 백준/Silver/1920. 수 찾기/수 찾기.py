fbf04b73b74bc023c2bacfbb9c17faf2e9055ec3
clwmfksek <139228254+clwmfksek@users.noreply.github.com>
2024-07-25 22:21:43 +0900

    [Silver IV] Title: 수 찾기, Time: 204 ms, Memory: 52392 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+n = int(input())
+arr = list(map(int, input().split()))
+m = int(input())
+arr2 = list(map(int,input().split()))
+
+arr3 = Counter(arr)
+
+for i in arr2:
+    if i in arr3: print(1)
+    else: print(0)
