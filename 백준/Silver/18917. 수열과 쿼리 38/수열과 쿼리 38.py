dc5ee0f03cde8f1fcbffedd6e858549745726eb5
clwmfksek <139228254+clwmfksek@users.noreply.github.com>
2024-08-08 14:36:39 +0900

    [Silver III] Title: 수열과 쿼리 38, Time: 288 ms, Memory: 113664 KB -BaekjoonHub

---
+++ 백준/Silver/18917. 수열과 쿼리 38/수열과 쿼리 38.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n=int(input())
+mysum=0
+xor=0
+for i in range(n):
+    oper=list(map(int,sys.stdin.readline().split()))
+    if oper[0]== 1:
+        mysum+=oper[1]
+        xor = xor^oper[1]
+    elif oper[0]== 2 :
+        mysum-=oper[1]
+        xor=xor^oper[1]
+    elif oper[0] == 3: print((mysum))
+    elif oper[0] == 4: print(xor)
