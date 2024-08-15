3e1a490e246660c7a43f0453d5315f9b9683e4f3
clwmfksek <139228254+clwmfksek@users.noreply.github.com>
2024-08-15 15:24:40 +0900

    [Gold V] Title: 수강 과목, Time: 264 ms, Memory: 179764 KB -BaekjoonHub

---
+++ 백준/Gold/17845. 수강 과목/수강 과목.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+T,N = map(int,input().split())
+K = [0]
+S = [0]
+dp = [[0]*(T+1) for i in range(N+1)]
+
+for i in range(N):
+    n1,n2 = map(int,input().split())
+    S.append(n1)
+    K.append(n2)
+
+for i in range(1,N+1):
+    for j in range(1,T+1):
+        if j >= K[i]:
+            dp[i][j] = max(dp[i-1][j],dp[i-1][j-K[i]]+S[i])
+        else :
+            dp[i][j] = dp[i-1][j]
+
+print(dp[N][T])
