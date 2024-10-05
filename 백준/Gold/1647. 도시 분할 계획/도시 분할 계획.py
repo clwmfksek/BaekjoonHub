1c615b8e7073112147df1f0ccdd61a5abee0b2fd
clwmfksek <139228254+clwmfksek@users.noreply.github.com>
2024-10-05 16:04:33 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1896 ms, Memory: 291956 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(3000)
+
+n,m = map(int,input().split())
+graph = []
+parent = [i for i in range(n+1)]
+
+def find(x):
+    if x == parent[x] : return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+def merge(a,b):
+    x = find(a)
+    y = find(b)
+    if(x > y): 
+        parent[x] = y
+    else: 
+        parent[y] = x
+
+sumd = 0
+for i in range(m):
+    n1, n2, n3 = map(int, input().split())
+    graph.append([n1, n2, n3])
+    sumd += n3
+
+graph.sort(key=lambda x: x[2])
+mst_weight = 0
+max_edge = 0
+
+for i in range(m):
+    s = graph[i][0]
+    e = graph[i][1]
+    w = graph[i][2]
+    
+    if find(s) != find(e):
+        merge(s, e)
+        mst_weight += w
+        max_edge = max(max_edge, w)
+
+print(mst_weight - max_edge)
