c89194227f327dda541896d03bcf3a3acd9a6915
clwmfksek <139228254+clwmfksek@users.noreply.github.com>
2024-11-05 09:47:54 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 280 ms, Memory: 255012 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,25 +1,28 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(3000)
+sys.setrecursionlimit(10**5)
 
-n,m = map(int,input().split())
+n, m = map(int, input().split())
 graph = []
 parent = [i for i in range(n+1)]
 
 def find(x):
-    if x == parent[x] : return x
-    parent[x] = find(parent[x])
+    if x != parent[x]:
+        parent[x] = find(parent[x])
     return parent[x]
 
-def merge(a,b):
+def merge(a, b):
     x = find(a)
     y = find(b)
-    if(x>y) : parent[x] = y
-    else : parent[y] = x
+    if x != y:
+        if x > y:
+            parent[x] = y
+        else:
+            parent[y] = x
 
 for i in range(m):
-    n1,n2,n3 = map(int,input().split())
-    graph.append([n1,n2,n3])
+    n1, n2, n3 = map(int, input().split())
+    graph.append([n1, n2, n3])
 
 graph.sort(key=lambda x: x[2])
 ans = 0
@@ -28,9 +31,11 @@ cnt = 0
 for i in range(m):
     s = graph[i][0]
     e = graph[i][1]
-    if(find(s) != find(e)):
-        merge(s,e)
+    if find(s) != find(e):
+        merge(s, e)
         ans += graph[i][2]
         cnt += 1
-    if cnt == n-1 : break
+    if cnt == n - 1:
+        break
+
 print(ans)
