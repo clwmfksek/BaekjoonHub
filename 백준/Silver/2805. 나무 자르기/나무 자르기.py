6efb1ec1319dad0ce7a91c76e7e203f0413b6357
Hwan <139228254+clwmfksek@users.noreply.github.com>
2025-10-27 17:37:41 +0900

    [Silver II] Title: 나무 자르기, Time: 440 ms, Memory: 258360 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,22 +1,19 @@
 import sys
+
 input = sys.stdin.readline
 
 n,m = map(int,input().split())
 lis = list(map(int,input().split()))
 
-st = 0
-en = max(lis)
-maxnum = 0
-
-while(st<=en):
-    total = 0
-    mid = (st+en)//2
-    for i in lis:
-        if i > mid:
-            total += i - mid
-    if total < m:
-        en = mid - 1
-    else :
-        maxnum = mid
-        st = mid + 1
-print(maxnum)
+lo = 0
+hi = 1e9+1
+while(lo <= hi):
+    mid = int((lo + hi) // 2)
+    cnt = 0
+    for i in range(n):
+        cnt += max(0,lis[i] - mid)
+    if(cnt >= m):
+        lo = mid + 1
+    else:
+        hi = mid - 1
+print(lo-1)
