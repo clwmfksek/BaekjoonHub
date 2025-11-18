99cd8d581bd5e42879f651be06e814563ac3f9df
Hwan <139228254+clwmfksek@users.noreply.github.com>
2025-11-18 11:53:32 +0900

    [Gold IV] Title: 적의 적, Time: 480 ms, Memory: 314144 KB -BaekjoonHub

---
+++ 백준/Gold/12893. 적의 적/적의 적.py
@@ -0,0 +1,32 @@
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+def dfs(node,p):
+    visited[node] = p
+    for i in graph[node]:
+        if not visited[i]:
+            a = dfs(i,-p)
+            if not a:
+                return False
+        elif visited[i] == visited[node]:
+            return False
+       
+    return True
+
+V,E = map(int,input().split())
+graph = [[] for i in range(V+1)]
+visited = [0] * (V+1)
+
+for i in range(E):
+    s,e = map(int,input().split())
+    graph[s].append(e)
+    graph[e].append(s)
+
+for i in range(1,V+1):
+    if not visited[i]:
+        res = dfs(i,1)
+        if not res :
+            break
+
+print(1 if res else 0)
