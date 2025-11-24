35c26c16840ae2b1f1629519a9de9fa61b1e0fc4
Hwan <139228254+clwmfksek@users.noreply.github.com>
2025-11-25 01:13:49 +0900

    [Gold IV] Title: Explorace, Time: 84 ms, Memory: 112228 KB -BaekjoonHub

---
+++ 백준/Gold/15835. Explorace/Explorace.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(3000)
+
+def find(x):
+    if x == parent[x] : return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+def merge(a,b):
+    x = find(a)
+    y = find(b)
+    if(x>y) : parent[x] = y
+    else : parent[y] = x
+
+for _ in range(int(input())):
+    n,m = map(int,input().split())
+    graph = []
+    parent = [i for i in range(n+1)]
+    
+    for i in range(m):
+        n1,n2,n3 = map(int,input().split())
+        graph.append([n1,n2,n3])
+        
+    graph.sort(key=lambda x: x[2])
+    ans = 0
+    cnt = 0
+
+    for i in range(m):
+        s = graph[i][0]
+        e = graph[i][1]
+        if(find(s) != find(e)):
+            merge(s,e)
+            ans += graph[i][2]
+            cnt += 1
+        if cnt == n-1 : break
+    print(f"Case #{_+1}: {ans} meters")
